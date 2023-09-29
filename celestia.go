d54009edfb6ca81f6645f36d319a35fe1df2d701
Javed Khan <tuxcanfly@gmail.com>
2023-09-29 12:19:29 -0700

    da: implement Commit

--- celestia.go
+++ celestia.go
@@ -1,33 +1,33 @@
 package celestia
 
 import (
-	"time"
 	"context"
+	"time"
 
-	"github.com/rollkit/go-da"
-	"github.com/rollkit/celestia-openrpc/types/share"
-	"github.com/rollkit/celestia-openrpc/types/blob"
 	openrpc "github.com/rollkit/celestia-openrpc"
+	"github.com/rollkit/celestia-openrpc/types/blob"
 	openrpcns "github.com/rollkit/celestia-openrpc/types/namespace"
+	"github.com/rollkit/celestia-openrpc/types/share"
+	"github.com/rollkit/go-da"
 	"github.com/rollkit/rollkit/log"
 )
 
 type Config struct {
-	AuthToken string `json:"auth_token"`
-	BaseURL string `json:"base_url"`
-	Timeout time.Duration `json:"timeout"`
-	Fee int64 `json:"fee"`
-	GasLimit uint64 `json:"gas_limit"`
+	AuthToken string        `json:"auth_token"`
+	BaseURL   string        `json:"base_url"`
+	Timeout   time.Duration `json:"timeout"`
+	Fee       int64         `json:"fee"`
+	GasLimit  uint64        `json:"gas_limit"`
 }
 
-/// CelestiaDA implements the celestia backend for the DA interface
+// / CelestiaDA implements the celestia backend for the DA interface
 type CelestiaDA struct {
-	rpc *openrpc.Client
-	height uint64
+	rpc       *openrpc.Client
+	height    uint64
 	namespace openrpcns.Namespace
-	config Config
-	logger log.Logger
-	ctx context.Context
+	config    Config
+	logger    log.Logger
+	ctx       context.Context
 }
 
 func (c *CelestiaDA) Get(ids []da.ID) ([]da.Blob, error) {
@@ -43,8 +43,24 @@ func (c *CelestiaDA) Get(ids []da.ID) ([]da.Blob, error) {
 	return blobs, nil
 }
 
-func (c *CelestiaDA) Commit(blobs []da.Blob) ([]da.Commitment, error) {
-	return nil, nil
+func (c *CelestiaDA) Commit(daBlobs []da.Blob) ([]da.Commitment, error) {
+	var blobs []*blob.Blob
+	for _, daBlob := range daBlobs {
+		b, err := blob.NewBlobV0(c.namespace.Bytes(), daBlob)
+		if err != nil {
+			return nil, err
+		}
+		blobs = append(blobs, b)
+	}
+	commitments, err := blob.CreateCommitments(blobs)
+	if err != nil {
+		return nil, err
+	}
+	var daCommitments []da.Commitment
+	for _, commitment := range commitments {
+		daCommitments = append(daCommitments, da.Commitment(commitment))
+	}
+	return daCommitments, nil
 }
 
 func (c *CelestiaDA) Submit(daBlobs []da.Blob) ([]da.ID, []da.Proof, error) {
