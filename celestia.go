f81c37ee1c82b19e8d86f0b113f81ec749e80745
Tomasz Zdybał <tomek@zdybal.lap.pl>
2023-10-18 01:18:50 +0200

    fix: fix multiple issues with DA implementation

--- celestia.go
+++ celestia.go
@@ -2,9 +2,7 @@ package celestia
 
 import (
 	"context"
-	"errors"
-	"fmt"
-
+	"encoding/binary"
 	"github.com/celestiaorg/celestia-app/x/blob/types"
 	rpc "github.com/celestiaorg/celestia-node/api/rpc/client"
 	"github.com/celestiaorg/celestia-node/blob"
@@ -12,21 +10,21 @@ import (
 	"github.com/celestiaorg/nmt"
 	"github.com/rollkit/go-da"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	"log"
+	"strings"
 )
 
 // CelestiaDA implements the celestia backend for the DA interface
 type CelestiaDA struct {
 	client    *rpc.Client
-	height    uint64
 	namespace share.Namespace
 	ctx       context.Context
 }
 
 // NewCelestiaDA returns an instance of CelestiaDA
-func NewCelestiaDA(client *rpc.Client, height uint64, namespace share.Namespace, ctx context.Context) *CelestiaDA {
+func NewCelestiaDA(client *rpc.Client, namespace share.Namespace, ctx context.Context) *CelestiaDA {
 	return &CelestiaDA{
 		client:    client,
-		height:    height,
 		namespace: namespace,
 		ctx:       ctx,
 	}
@@ -35,8 +33,8 @@ func NewCelestiaDA(client *rpc.Client, height uint64, namespace share.Namespace,
 func (c *CelestiaDA) Get(ids []da.ID) ([]da.Blob, error) {
 	var blobs []da.Blob
 	for _, id := range ids {
-		// TODO: extract commitment from ID
-		blob, err := c.client.Blob.Get(c.ctx, c.height, c.namespace, blob.Commitment(id))
+		height, commitment := splitID(id)
+		blob, err := c.client.Blob.Get(c.ctx, height, c.namespace, commitment)
 		if err != nil {
 			return nil, err
 		}
@@ -47,16 +45,16 @@ func (c *CelestiaDA) Get(ids []da.ID) ([]da.Blob, error) {
 
 func (c *CelestiaDA) GetIDs(height uint64) ([]da.ID, error) {
 	var ids []da.ID
-	blobs, err := c.client.Blob.GetAll(c.ctx, c.height, []share.Namespace{c.namespace})
-	if errors.Is(err, blob.ErrBlobNotFound) {
-		return nil, nil
-	}
+	blobs, err := c.client.Blob.GetAll(c.ctx, height, []share.Namespace{c.namespace})
 	if err != nil {
+		if strings.Contains(err.Error(), blob.ErrBlobNotFound.Error()) {
+			return nil, nil
+		}
 		return nil, err
 	}
 	for _, blob := range blobs {
 		// TODO: commitment -> id
-		ids = append(ids, blob.Commitment)
+		ids = append(ids, makeID(height, blob.Commitment))
 	}
 	return ids, nil
 }
@@ -83,19 +81,37 @@ func (c *CelestiaDA) Commit(daBlobs []da.Blob) ([]da.Commitment, error) {
 
 func (c *CelestiaDA) Submit(daBlobs []da.Blob) ([]da.ID, []da.Proof, error) {
 	var blobs []*blob.Blob
+	var commitments []da.Commitment
 	for _, daBlob := range daBlobs {
 		b, err := blob.NewBlobV0(c.namespace, daBlob)
 		if err != nil {
 			return nil, nil, err
 		}
 		blobs = append(blobs, b)
+
+		commitment, err := types.CreateCommitment(&b.Blob)
+		commitments = append(commitments, commitment)
 	}
 	height, err := c.client.Blob.Submit(c.ctx, blobs, blob.DefaultSubmitOptions())
 	if err != nil {
 		return nil, nil, err
 	}
-	fmt.Println("succesfully submitted blobs", "height", height)
-	return nil, nil, nil
+	log.Println("successfully submitted blobs", "height", height)
+	ids := make([]da.ID, len(daBlobs))
+	proofs := make([]da.Proof, len(daBlobs))
+	for i, commitment := range commitments {
+		ids[i] = makeID(height, commitment)
+		proof, err := c.client.Blob.GetProof(c.ctx, height, c.namespace, commitment)
+		if err != nil {
+			return nil, nil, err
+		}
+		// TODO(tzdybal): does always len(*proof) == 1?
+		proofs[i], err = (*proof)[0].MarshalJSON()
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+	return ids, proofs, nil
 }
 
 func (c *CelestiaDA) Validate(ids []da.ID, daProofs []da.Proof) ([]bool, error) {
@@ -110,14 +126,29 @@ func (c *CelestiaDA) Validate(ids []da.ID, daProofs []da.Proof) ([]bool, error) 
 		proofs = append(proofs, proof)
 	}
 	for i, id := range ids {
-		// TODO: extract commitment from ID
-		isIncluded, err := c.client.Blob.Included(c.ctx, c.height, share.Namespace(c.namespace), proofs[i], blob.Commitment(id))
-		if err != nil {
-			return nil, err
-		}
+		height, commitment := splitID(id)
+		// TODO(tzdybal): for some reason, if proof doesn't match commitment, API returns (false, "blob: invalid proof")
+		//    but analysis of the code in celestia-node implies this should never happen - maybe it's caused by openrpc?
+		//    there is no way of gently handling errors here, but returned value is fine for us
+		isIncluded, err := c.client.Blob.Included(c.ctx, height, c.namespace, proofs[i], commitment)
+		log.Println("Included: ", isIncluded, err)
 		included = append(included, isIncluded)
 	}
 	return included, nil
+}
+
+func makeID(height uint64, commitment da.Commitment) da.ID {
+	id := make([]byte, 8+len(commitment))
+	binary.LittleEndian.PutUint64(id, height)
+	copy(id[8:], commitment)
+	return id
+}
+
+func splitID(id da.ID) (uint64, da.Commitment) {
+	if len(id) <= 8 {
+		return 0, nil
+	}
+	return binary.LittleEndian.Uint64(id[:8]), id[8:]
 }
 
 var _ da.DA = &CelestiaDA{}
