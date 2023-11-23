3b113a17fcf337f0a0d9a76800602ea1ffb3212d
Javed Khan <tuxcanfly@gmail.com>
2023-11-23 11:10:38 -0800

    docker: entrypoint.sh echo

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,10 +6,10 @@ if [ "$1" = 'celestia-da' ]; then
     echo "Initializing Celestia Node with command:"
 
     if [[ -n "$NODE_STORE" ]]; then
-        echo "celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}" --node.store "${NODE_STORE}""
+        echo "celestia-da "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}" --node.store "${NODE_STORE}""
         celestia-da "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}" --node.store "${NODE_STORE}"
     else
-        echo "celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}""
+        echo "celestia-da "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}""
         celestia-da "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}"
     fi
 
